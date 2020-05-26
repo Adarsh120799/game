d91feb84111eeaaf624ed9c1e606104486319b63
adarsh <adarshsingh120799@gmail.com>
2020-05-26 06:35:12 +0530

    this

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const request = require('request')
 const app = express()
-
-
 app.use(express.static('dice'));
 app.use(express.static('images'))
 app.get('/',(req,res)=>{
 res.sendFile(__dirname+"/dice.html")
-
 })
 
 app.listen(process.env.PORT ||5000,()=>{
